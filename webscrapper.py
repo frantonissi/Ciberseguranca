c306d7a7762c864565a88bde226ca35ad7c1f3cb
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2025-11-11 17:06:19 -0300

    projeto de script para procurar links ocultos

---
+++ webscrapper.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+url_base = "http://books.toscrape.com/"
+
+pagina = requests.get(url_base) #visitar um site
+
+soup = BeautifulSoup(pagina.text, "html.parser")
+
+links = soup.find_all("a")
+
+for link in links: #Dentro do loop, cada tag <a> se comporta como um dicionário do Python. Podemos pegar o atributo href usando colchetes, assim: link['href'].
+    
+    # 'link' é cada item <a>, um de cada vez
+    # Agora pegamos o 'href' do item atual
+    
+    #Temos as tags <a>, mas não queremos a tag inteira. Nós só queremos o link de verdade, que está no atributo href.
+    
+    href_original = link['href']
+    
+    if href_original: # Só processa se o 'href' existir
+        href_consertado = urljoin(url_base, href_original)
+        print(href_consertado)
+
+print("\n--- Extração concluída ---")

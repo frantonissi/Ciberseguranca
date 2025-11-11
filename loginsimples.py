1d524244394fbe402b41a025840dcd77745f9808
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2025-11-11 16:00:28 -0300

    Refactor access control logic and clean up code formatting

--- loginsimples.py
+++ loginsimples.py
@@ -3,6 +3,7 @@ import hashlib
 hash_Francisco = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"
 hash_Maria = "dbff5341acad5e2a58db4efd5e72e2d9a0a843a28e02b1183c68162d0a3a3de6"
 
+
 nome = input("Qual é o seu nome? ")
 senha = input("Digite a senha: ")
 
@@ -12,12 +13,32 @@ senha = input("Digite a senha: ")
 #    - .hexdigest() nos dá a "vitamina" (o hash) em texto
 hash_gerado = hashlib.sha256(senha.encode('utf-8')).hexdigest()
 
-print(f"A senha vira este hash:")
-print(hash_gerado)
+#print(f"A senha vira este hash:")
+#print(hash_gerado)
+
+nivel_acesso = ""
 
 if(nome == "Francisco" and hash_gerado == hash_Francisco):
     print("senha correta, acesso permitido")
+    nivel_acesso = "admin"
+    
 elif(nome == "Maria" and hash_gerado == hash_Maria ):
     print("senha correta, acesso permitido")
+    nivel_acesso = "user"
+
 else:
     print("acesso negado")
+    
+if (nivel_acesso == "admin"):
+    print("Bem vindo administrador\n")
+    print("\n--- Ações do Admin ---")
+    print("1. Deletar usuários")
+    print("2. Ver logs")
+    
+elif(nivel_acesso == "user"):
+    print ("Bem vindo usuario\n")
+    print("\n--- Ações do Usuário ---")
+    print("1. Ver perfil")
+    
+
+    

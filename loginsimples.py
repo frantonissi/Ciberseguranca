ca3a1114418c86cafb62f131260e0a636a2bf094
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2025-11-11 16:05:50 -0300

    Refactor login verification logic for improved readability and structure

--- loginsimples.py
+++ loginsimples.py
@@ -1,33 +1,41 @@
 import hashlib
+def verificar_login(nome, senha):
+    
+    hash_Francisco = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"
+    hash_Maria = "dbff5341acad5e2a58db4efd5e72e2d9a0a843a28e02b1183c68162d0a3a3de6"
 
-hash_Francisco = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"
-hash_Maria = "dbff5341acad5e2a58db4efd5e72e2d9a0a843a28e02b1183c68162d0a3a3de6"
 
-
-nome = input("Qual é o seu nome? ")
-senha = input("Digite a senha: ")
+    nome = input("Qual é o seu nome? ")
+    senha = input("Digite a senha: ")
 
 # 2. O "liquidificador" (SHA-256)
 #    - .encode() transforma a string em bytes
 #    - hashlib.sha256() processa esses bytes
 #    - .hexdigest() nos dá a "vitamina" (o hash) em texto
-hash_gerado = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+    hash_gerado = hashlib.sha256(senha.encode('utf-8')).hexdigest()
 
 #print(f"A senha vira este hash:")
 #print(hash_gerado)
 
-nivel_acesso = ""
+    nivel_acesso = ""
 
-if(nome == "Francisco" and hash_gerado == hash_Francisco):
-    print("senha correta, acesso permitido")
-    nivel_acesso = "admin"
+    if(nome == "Francisco" and hash_gerado == hash_Francisco):
+        print("senha correta, acesso permitido")
+        nivel_acesso = "admin"
     
-elif(nome == "Maria" and hash_gerado == hash_Maria ):
-    print("senha correta, acesso permitido")
-    nivel_acesso = "user"
+    elif(nome == "Maria" and hash_gerado == hash_Maria ):
+        print("senha correta, acesso permitido")
+        nivel_acesso = "user"
 
-else:
-    print("acesso negado")
+    else:
+        print("acesso negado")
+    
+    
+    
+    
+    return nivel_acesso
+
+
     
 if (nivel_acesso == "admin"):
     print("Bem vindo administrador\n")

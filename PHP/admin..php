6fdbfba18d3e6dfbfc42bec6c7a811a06a3cd833
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-01-07 14:36:27 -0300

    Refactor admin authentication script: streamline database connection and user input handling

--- PHP/admin..php
+++ PHP/admin..php
@@ -1,14 +1,24 @@
+
 <?php
 
-$_GET["email"];
-$_GET["senha"];
+$con = "mysql:host=localhost;dbname:exemplo";
+$pdo = new PDO($con, "admin" , "senhafoda123)");
+
+$email = $_GET["email"];
+$senha = $_GET["senha"];
 
 //echo $email . " " . $senha;
 
-if($email == "admin" and $senha == "admin123") {
-    echo " " . "Bem vindo administrador";
-}else{
-    echo "Acesso negado";
-}
+//if ($email == "admin" and $senha == "admin123"){
+//	echo " " .  "Bem vindo administrador";
+//}else {
+//	echo "Vc nao pode acessar";
+//}
+
+$sql = $pdo->prepare("select * from usuarios where usuario = '".$email."' and senha = '".$senha."';");
+$sql->execute();
+$linhas = $sql->fetchAll();
+
+print_r($linhas);
 
 ?>

a934726a5d795e46e0407b8b2c58e110c21e4794
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-02-08 01:09:59 -0300

    novo commit

--- Python/cliente_ssh.py
+++ Python/cliente_ssh.py
@@ -10,11 +10,13 @@ senha = getpass.getpass('Digite a senha: ')
 ssh = paramiko.SSHClient()
 
 ssh.connect(host, port, username, senha)
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 
 stdin, stdout, stderr = ssh.exec_command('ls') #retorna 3 valores: STDIN, STDOUT,STDERR 
 #STDIN = o valor do comando que enviou
 #STDOUT = o valor da saido do comando
 
 print(stdout.read())
+
 
 
